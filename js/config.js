b85f4f5dd87672414c5e347727e7ec58c876add2
IgNiKoN <igorkondratiev89@gmail.com>
2026-04-29 12:08:35 +0300

    Delete js/config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-/* Файл: js/config.js */
-window.APP_CONFIG = {
-    // ВСТАВЬ СЮДА СВОИ ДАННЫЕ ИЗ SUPABASE (Project Settings -> API)
-    SUPABASE_URL: 'https://kocqncvpcpjnadjibjzv.supabase.co',
-    SUPABASE_KEY: 'sb_publishable_OPnGjLG5HRV06COKUB9A-A_LubcBrfw'
-};
